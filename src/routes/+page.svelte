946c8f0557dbc40103951a3eeb2e487e5c29a569
= <matdombrock@gmail.com>
2022-12-08 20:05:48 -0800

    Update title

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -237,7 +237,7 @@ s > 0 ? 1 : -1;`,
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>HTML 5 Boilerplate</title>
+    <title>ReplicatAudio DSP Tool</title>
     <link rel="stylesheet" href="style.css">
   </head>
   <body>
