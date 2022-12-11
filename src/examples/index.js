5f283dfcedee57bb50389775919c11ef81add606
Mathieu Dombrock <matdombrock@gmail.com>
2022-12-11 00:12:19 -0800

    add pwm example

--- src/examples/index.js
+++ src/examples/index.js
@@ -4,6 +4,7 @@ import square from './square.json';
 import clipping from './clipping.json';
 import tanTri from './tanTri.json';
 import saw from './saw.json';
+import pwm from './pwm.json';
 import waveTable1 from './wavetable1.json';
 import waveTable2 from './wavetable2.json';
 import buffer from './buffer.json';
@@ -15,6 +16,7 @@ export const examples = {
     'Clipping': clipping,
     'Tan Triangle': tanTri,
     'Sawtooth': saw,
+    'Pulse Width': pwm,
     'Wavetable 1': waveTable1,
     'Wavetable 2': waveTable2,
     'Buffer': buffer,
