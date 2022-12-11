3f932a92fd71f78c2de00225088053c70a11bcdb
= <matdombrock@gmail.com>
2022-12-11 01:08:29 -0800

    move plot to own comp

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,45 +1,7 @@
 <script>
     import { onMount } from 'svelte'
-	
-	onMount(async () => {
-        ace.config.set('basePath', 'https://cdn.jsdelivr.net/npm/ace-builds@1.13.2/src-noconflict/');
-        ast.editor = ace.edit("editor");
-        // ace.edit(editor, {
-        //     mode: "ace/mode/javascript",
-        //     selectionStyle: "text"
-        // });
-        //ast.editor.setTheme("ace/theme/dracula");
-        //ast.editor.setTheme("ace/theme/one_dark");
-        ast.editor.setTheme("ace/theme/pastel_on_dark");
-        //editor.resize();
-        //editor.setTheme("ace/theme/solarized");
-        ast.editor.session.setMode("ace/mode/javascript");
-        ast.editor.setOptions({
-            selectionStyle: "text",
-            //autoScrollEditorIntoView: true,
-            //copyWithEmptySelection: true,
-            //animatedScroll: false,
-            //showPrintMargin: false,
-            //scrollPastEnd: false,
-            //minLines: 10,
-            //dragEnabled: false,
-            //hScrollBarAlwaysVisible: true,
-            //vScrollBarAlwaysVisible: true,
-        });
-        ast.editor.setValue(st.userJS);
-        setInterval(function(){
-            if(ast.useAce)
-            {
-                st.userJS = ast.editor.getValue();
-            }
-        }, 333);
-	})
-    
-    function resizeEditor()
-    {
-        editor.resize();
-    }
     import Clipboard from "svelte-clipboard";
+    import Plot from "./plot.svelte";
     import { stLS } from "./store.js";
     import {examples} from "../examples";
     const cfg = {
@@ -79,6 +41,46 @@ s > 0 ? 1 : -1;`,
     };
     // Create a deep copy of the st object for restoring
     const stb = JSON.parse(JSON.stringify(st));
+
+    onMount(async () => {
+        ace.config.set('basePath', 'https://cdn.jsdelivr.net/npm/ace-builds@1.13.2/src-noconflict/');
+        ast.editor = ace.edit("editor");
+        // ace.edit(editor, {
+        //     mode: "ace/mode/javascript",
+        //     selectionStyle: "text"
+        // });
+        //ast.editor.setTheme("ace/theme/dracula");
+        //ast.editor.setTheme("ace/theme/one_dark");
+        ast.editor.setTheme("ace/theme/pastel_on_dark");
+        //editor.resize();
+        //editor.setTheme("ace/theme/solarized");
+        ast.editor.session.setMode("ace/mode/javascript");
+        ast.editor.setOptions({
+            selectionStyle: "text",
+            //autoScrollEditorIntoView: true,
+            //copyWithEmptySelection: true,
+            //animatedScroll: false,
+            //showPrintMargin: false,
+            //scrollPastEnd: false,
+            //minLines: 10,
+            //dragEnabled: false,
+            //hScrollBarAlwaysVisible: true,
+            //vScrollBarAlwaysVisible: true,
+        });
+        ast.editor.setValue(st.userJS);
+        setInterval(function(){
+            if(ast.useAce)
+            {
+                st.userJS = ast.editor.getValue();
+            }
+        }, 333);
+	})
+    
+    function resizeEditor()
+    {
+        editor.resize();
+    }
+
     function resetState(){
         if(confirm("Reset all code and paramters?"))
         {
@@ -108,14 +110,6 @@ s > 0 ? 1 : -1;`,
         else{
             alert('No Data To Load!');
         }
-    }
-    function showModal(type){
-        ast.modal.type = type;
-        ast.modal.show = true;
-    }
-    function closeModal()
-    {
-        ast.modal.show = false;
     }
     function loadExample(json)
     {
@@ -167,164 +161,21 @@ s > 0 ? 1 : -1;`,
             }
         };
     }
+    //
+    function showModal(type){
+        ast.modal.type = type;
+        ast.modal.show = true;
+    }
+    function closeModal()
+    {
+        ast.modal.show = false;
+    }
+    //
     function setup(skipLS=false){
         st.paramVals.length = cfg.paramCount;
         st.paramVals.fill(0.5);
-        // if(!skipLS)
-        // {
-        //     loadFromLS(false);
-        // }
-        
     }
     setup();
-
-    // Wave runner
-    function wave(x,s,p)
-    {
-        const js = st.userJS;
-        try{
-            st.errStr = "";
-            st.hasErr = false;
-            let w = [];
-            w.length = cfg.maxWaves;
-            // Math vars
-            const pi = Math.PI;
-            const pi2 = Math.PI*2;
-            const halfPi = Math.PI/2;
-            const sin = Math.sin;
-            const cos = Math.cos;
-            const abs = Math.abs;
-            const tan = Math.tan;
-            const tanh = Math.tanh;
-            const round = Math.round;
-            const floor = Math.floor;
-            const ceil = Math.ceil;
-            const random = Math.random;
-
-            const xr = x/pi2;
-
-            const we = eval(js);
-            w[0] = w[0] === undefined ? we : w[0];
-            return w;
-        }
-        catch(err)
-        {
-            if(st.errStr !== err)
-            {
-                //console.log(err);
-            }
-            st.errStr = err;
-            st.hasErr = true;
-            return 0;
-        }
-    }
-
-    // Canvas
-    import { Canvas, Layer, t } from "svelte-canvas";
-
-    $: render = ({ context, width, height }) => {
-        const ctx = context;// cant rename in render func
-        const w = 640;
-        const h = 320;
-        const hc = h/2;
-
-        const amp = st.amplitude/2;
-
-        const gs = st.gridSize*2;
-        const gx = w/(st.periods*gs);
-        const gy = h/gs;
-
-        ctx.clearRect(0,0,w,h);
-
-        if(st.showGrid)
-        {
-            for (let i =0; i < st.periods*gs; i++)
-            {
-                ctx.beginPath();
-                ctx.setLineDash([]);
-                ctx.moveTo(gx*(i+1),0);
-                ctx.lineTo(gx*(i+1),h);
-                ctx.strokeStyle = "rgb(55,50,55)";
-                ctx.lineWidth = 1;
-                ctx.stroke();
-                ctx.closePath();
-            }
-            for (let i =0; i < gs; i++)
-            {
-                ctx.beginPath();
-                ctx.setLineDash([]);
-                ctx.moveTo(0,gy*(i+1));
-                ctx.lineTo(w,gy*(i+1));
-                ctx.strokeStyle = "rgb(55,50,55)";
-                ctx.lineWidth = 1;
-                ctx.stroke();
-                ctx.closePath();
-            }
-        }
-        
-
-        // Could add an optimization to only run active waves
-        for(let wi = 0; wi < cfg.maxWaves; wi++)
-        {
-            ctx.beginPath();
-            ctx.setLineDash([]);
-            ctx.lineWidth = 2;
-            ctx.moveTo(0, hc);
-            for(let i = 0; i < w; i++)
-            {
-                const x = i/(w/(Math.PI*(st.periods*2)));
-                const s = Math.sin(x);
-                if(i===0)
-                {
-                    // Dont draw a line on first move
-                    ctx.moveTo(i,hc+(-wave(x,s, st.paramVals)[wi]*(amp*hc)));
-                }
-                else{
-                    ctx.lineTo(i,hc+(-wave(x,s, st.paramVals)[wi]*(amp*hc)));
-                }
-            }
-            const strokeStyles = [
-                "rgb(200,255,255",
-                "rgb(255,200,255)",
-                "rgb(255,255,200)",
-                
-                "rgb(255,255,255)",
-
-                "rgb(255,200,255)",
-                "rgb(255,255,200)",
-                "rgb(200,255,255",
-                "rgb(255,255,255)",
-
-                "rgb(255,200,255)",
-                "rgb(255,255,200)",
-                "rgb(200,255,255",
-                "rgb(255,255,255)",
-
-                "rgb(255,200,255)",
-                "rgb(255,255,200)",
-                "rgb(200,255,255",
-                "rgb(255,255,255)"
-            ];
-            ctx.strokeStyle = strokeStyles[wi];           
-            ctx.stroke();
-            ctx.closePath();
-        }
-        if(st.showRefSine)
-        {
-            ctx.beginPath();
-            ctx.setLineDash([5, 15]);
-            ctx.lineWidth = 1;
-            // ref sine
-            for(let i = 0; i < w; i++)
-            {
-                const x = i/(w/(Math.PI*(st.periods*2)));
-                const s = Math.sin(x);
-                ctx.lineTo(i,hc+(-s*(amp*hc)));
-            }
-            ctx.strokeStyle = "rgb(255,225,255)";
-            ctx.stroke();
-        }
-    };
 </script>
 <svelte:head>
 	<script src="https://cdn.jsdelivr.net/npm/ace-builds@1.13.2/src-noconflict/ace.min.js" on:load={setup}></script>
@@ -419,9 +270,10 @@ s > 0 ? 1 : -1;`,
             </a>
         </div>
         <br>
-        <Canvas width={640} height={320}>
+        <Plot st={st} cfg={cfg} />
+        <!-- <Canvas width={640} height={320}>
             <Layer {render} />
-        </Canvas>
+        </Canvas> -->
         <br>
         <div class="scrollArea">
             
---
+++ src/routes/plot.svelte
@@ -0,0 +1,153 @@
+<script>
+    import { Canvas, Layer, t } from "svelte-canvas";
+    export let st;
+    export let cfg;
+    // Wave runner
+    function wave(x,s,p)
+    {
+        const js = st.userJS;
+        try{
+            st.errStr = "";
+            st.hasErr = false;
+            let w = [];
+            w.length = cfg.maxWaves;
+            // Math vars
+            const pi = Math.PI;
+            const pi2 = Math.PI*2;
+            const halfPi = Math.PI/2;
+            const sin = Math.sin;
+            const cos = Math.cos;
+            const abs = Math.abs;
+            const tan = Math.tan;
+            const tanh = Math.tanh;
+            const round = Math.round;
+            const floor = Math.floor;
+            const ceil = Math.ceil;
+            const random = Math.random;
+
+            const xr = x/pi2;
+
+            const we = eval(js);
+            w[0] = w[0] === undefined ? we : w[0];
+            return w;
+        }
+        catch(err)
+        {
+            if(st.errStr !== err)
+            {
+                //console.log(err);
+            }
+            st.errStr = err;
+            st.hasErr = true;
+            return 0;
+        }
+    }
+    $: render = ({ context, width, height }) => {
+        const ctx = context;// cant rename in render func
+        const w = 640;
+        const h = 320;
+        const hc = h/2;
+
+        const amp = st.amplitude/2;
+
+        const gs = st.gridSize*2;
+        const gx = w/(st.periods*gs);
+        const gy = h/gs;
+
+        ctx.clearRect(0,0,w,h);
+
+        if(st.showGrid)
+        {
+            for (let i =0; i < st.periods*gs; i++)
+            {
+                ctx.beginPath();
+                ctx.setLineDash([]);
+                ctx.moveTo(gx*(i+1),0);
+                ctx.lineTo(gx*(i+1),h);
+                ctx.strokeStyle = "rgb(55,50,55)";
+                ctx.lineWidth = 1;
+                ctx.stroke();
+                ctx.closePath();
+            }
+            for (let i =0; i < gs; i++)
+            {
+                ctx.beginPath();
+                ctx.setLineDash([]);
+                ctx.moveTo(0,gy*(i+1));
+                ctx.lineTo(w,gy*(i+1));
+                ctx.strokeStyle = "rgb(55,50,55)";
+                ctx.lineWidth = 1;
+                ctx.stroke();
+                ctx.closePath();
+            }
+        }
+        
+
+        // Could add an optimization to only run active waves
+        for(let wi = 0; wi < cfg.maxWaves; wi++)
+        {
+            ctx.beginPath();
+            ctx.setLineDash([]);
+            ctx.lineWidth = 2;
+            ctx.moveTo(0, hc);
+            for(let i = 0; i < w; i++)
+            {
+                const x = i/(w/(Math.PI*(st.periods*2)));
+                const s = Math.sin(x);
+                if(i===0)
+                {
+                    // Dont draw a line on first move
+                    ctx.moveTo(i,hc+(-wave(x,s, st.paramVals)[wi]*(amp*hc)));
+                }
+                else{
+                    ctx.lineTo(i,hc+(-wave(x,s, st.paramVals)[wi]*(amp*hc)));
+                }
+            }
+            const strokeStyles = [
+                "rgb(200,255,255",
+                "rgb(255,200,255)",
+                "rgb(255,255,200)",
+                
+                "rgb(255,255,255)",
+
+                "rgb(255,200,255)",
+                "rgb(255,255,200)",
+                "rgb(200,255,255",
+                "rgb(255,255,255)",
+
+                "rgb(255,200,255)",
+                "rgb(255,255,200)",
+                "rgb(200,255,255",
+                "rgb(255,255,255)",
+
+                "rgb(255,200,255)",
+                "rgb(255,255,200)",
+                "rgb(200,255,255",
+                "rgb(255,255,255)"
+            ];
+            ctx.strokeStyle = strokeStyles[wi];           
+            ctx.stroke();
+            ctx.closePath();
+        }
+        if(st.showRefSine)
+        {
+            ctx.beginPath();
+            ctx.setLineDash([5, 15]);
+            ctx.lineWidth = 1;
+            // ref sine
+            for(let i = 0; i < w; i++)
+            {
+                const x = i/(w/(Math.PI*(st.periods*2)));
+                const s = Math.sin(x);
+                ctx.lineTo(i,hc+(-s*(amp*hc)));
+            }
+            ctx.strokeStyle = "rgb(255,225,255)";
+            ctx.stroke();
+        }
+    };
+</script>
+<div>
+    <Canvas width={640} height={320}>
+        <Layer {render} />
+    </Canvas>
+</div>
